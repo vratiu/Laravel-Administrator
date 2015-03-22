5d0799f37af9b137e8328dfaa082544dce7a5b06
Virgil Ratiu <virgil@cloud-pixel.com>
2015-03-22 18:43:35 +0200

    Enabled numeric keys in Enum field

--- src/Frozennode/Administrator/Fields/Enum.php
+++ src/Frozennode/Administrator/Fields/Enum.php
@@ -30,7 +30,7 @@ class Enum extends Field {
 		foreach ($dataOptions as $val => $text)
 		{
 			$options['options'][] = array(
-				'id' => is_numeric($val) ? $text : $val,
+				'id' => is_int($val) ? $text : $val,
 				'text' => $text,
 			);
 		}
