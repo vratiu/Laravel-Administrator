a760d3bc2597f9c9153cedd32e2f113d47df8d3a
Virgil Ratiu <virgil@cloud-pixel.com>
2015-03-22 19:32:36 +0200

    Fixed Enum filter problem
    Fo 0 values

--- src/Frozennode/Administrator/Fields/Enum.php
+++ src/Frozennode/Administrator/Fields/Enum.php
@@ -5,83 +5,84 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Enum extends Field {
 
-	/**
-	 * The options used for the enum field
-	 *
-	 * @var array
-	 */
-	protected $rules = array(
-		'options' => 'required|array|not_empty',
-	);
+    /**
+     * The options used for the enum field
+     *
+     * @var array
+     */
+    protected $rules = array(
+        'options' => 'required|array|not_empty',
+    );
 
-	/**
-	 * Builds a few basic options
-	 */
-	public function build()
-	{
-		parent::build();
+    /**
+     * Builds a few basic options
+     */
+    public function build()
+    {
+        parent::build();
 
-		$options = $this->suppliedOptions;
+        $options = $this->suppliedOptions;
 
-		$dataOptions = $options['options'];
-		$options['options'] = array();
+        $dataOptions = $options['options'];
+        $options['options'] = array();
 
-		//iterate over the options to create the options assoc array
-		foreach ($dataOptions as $val => $text)
-		{
-			$options['options'][] = array(
-				'id' => $val,
-				'text' => $text,
-			);
-		}
+        //iterate over the options to create the options assoc array
+        foreach ($dataOptions as $val => $text)
+        {
+            $options['options'][] = array(
+                'id' => $val,
+                'text' => $text,
+            );
+        }
 
-		$this->suppliedOptions = $options;
-	}
+        $this->suppliedOptions = $options;
+    }
 
-	/**
-	 * Fill a model with input data
-	 *
-	 * @param \Illuminate\Database\Eloquent\model	$model
-	 * @param mixed									$input
-	 */
-	public function fillModel(&$model, $input)
-	{
-		$model->{$this->getOption('field_name')} = $input;
-	}
+    /**
+     * Fill a model with input data
+     *
+     * @param \Illuminate\Database\Eloquent\model	$model
+     * @param mixed									$input
+     */
+    public function fillModel(&$model, $input)
+    {
+        $model->{$this->getOption('field_name')} = $input;
+    }
 
-	/**
-	 * Sets the filter options for this item
-	 *
-	 * @param array		$filter
-	 *
-	 * @return void
-	 */
-	public function setFilter($filter)
-	{
-		parent::setFilter($filter);
+    /**
+     * Sets the filter options for this item
+     *
+     * @param array		$filter
+     *
+     * @return void
+     */
+    public function setFilter($filter)
+    {
+        parent::setFilter($filter);
 
-		$this->userOptions['value'] = $this->getOption('value') === '' ? null : $this->getOption('value');
-	}
+        $this->userOptions['value'] = $this->getOption('value') === '' ? null : $this->getOption('value');
+    }
 
-	/**
-	 * Filters a query object
-	 *
-	 * @param \Illuminate\Database\Query\Builder	$query
-	 * @param array									$selects
-	 *
-	 * @return void
-	 */
-	public function filterQuery(QueryBuilder &$query, &$selects = null)
-	{
-		//run the parent method
-		parent::filterQuery($query, $selects);
+    /**
+     * Filters a query object
+     *
+     * @param \Illuminate\Database\Query\Builder	$query
+     * @param array									$selects
+     *
+     * @return void
+     */
+    public function filterQuery(QueryBuilder &$query, &$selects = null)
+    {
+        //run the parent method
+        parent::filterQuery($query, $selects);
 
-		//if there is no value, return
-		if (!$this->getOption('value'))
-		{
-			return;
-		}
+        //if there is no value, return
+        $value = $this->getOption('value');
+        if ($value === false || is_null($value) || (is_string($value) && strlen($value) == 0))
+        {
+            return;
+        }
 
-		$query->where($this->config->getDataModel()->getTable().'.'.$this->getOption('field_name'), '=', $this->getOption('value'));
-	}
+        $query->where($this->config->getDataModel()->getTable().'.'.$this->getOption('field_name'), '=', $this->getOption('value'));
+    }
 }
